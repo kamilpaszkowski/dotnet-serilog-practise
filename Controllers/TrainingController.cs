4475c5bd29cbde13975cc820b8ea86e50442562f
Kamil Paszkowski <Kamil.Paszkowski@euvic.pl>
2022-08-14 11:42:23 +0200

    Add project files.

---
+++ Controllers/TrainingController.cs
@@ -0,0 +1,30 @@
+﻿namespace Euvic.Training.Serilog.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    namespace Euvic.Training.Serilog.Controllers
+    {
+        [Route("api/trainings")]
+        [ApiController]
+        public class TrainingController : ControllerBase
+        {
+            private readonly ILogger<TrainingController> _logger;
+
+            public TrainingController(ILogger<TrainingController> logger)
+            {
+                _logger = logger;
+            }
+
+            [HttpGet]
+            public IActionResult GetTrainings(string name, int page)
+            {
+                _logger.LogInformation($"Searching trainings that contain '{name}'. Take page number {page}");
+                _logger.LogWarning("Searching trainings that contain '{name}'. Take page number {page}", name, page);
+
+                return Ok();
+            }
+        }
+    }
+
+}
